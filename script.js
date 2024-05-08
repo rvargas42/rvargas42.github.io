bae85507db02911ebfff1ea9defd67c3d7a29b4b
rvargas42 <ravargas.42t@gmail.com>
2024-05-09 00:58:08 +0200

    improved toggle buttons

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function toggleContent(button) {
     var content = button.nextElementSibling;
     content.style.display = (content.style.display === 'none') ? 'block' : 'none';
 }
+
+function stopFlash(element){
+	element.style.animation = "none";
+}
+
+
+function mostrarPDF(ruta) {
+  // Obtiene el iframe
+  var iframe = document.getElementById('pdfIframe');
+  // Cambia el src del iframe
+  iframe.src = ruta;
+  // Muestra el iframe
+  iframe.style.display = 'block';
+}
